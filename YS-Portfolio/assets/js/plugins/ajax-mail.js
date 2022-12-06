37424d58a023a06697239ead56fd16eab94e29c0
Yasmine Salim <ysalim@uwo.ca>
2022-12-06 08:20:30 -0500

    updated

--- YS-Portfolio/assets/js/plugins/ajax-mail.js
+++ YS-Portfolio/assets/js/plugins/ajax-mail.js
@@ -1,48 +1,29 @@
+
 $(function() {
-
-	// Get the form.
-	var form = $('#contact-form');
-
-	// Get the messages div.
-	var formMessages = $('.form-messege');
-
-	// Set up an event listener for the contact form.
-	$(form).submit(function(e) {
-		// Stop the browser from submitting the form.
-		e.preventDefault();
-
-		// Serialize the form data.
-		var formData = $(form).serialize();
-
-		// Submit the form using AJAX.
-		$.ajax({
-			type: 'POST',
-			url: $(form).attr('action'),
-			data: formData
-		})
-		.done(function(response) {
-			// Make sure that the formMessages div has the 'success' class.
-			$(formMessages).removeClass('error');
-			$(formMessages).addClass('success');
-
-			// Set the message text.
-			$(formMessages).text(response);
-
-			// Clear the form.
-			$('#contact-form input,#contact-form textarea').val('');
-		})
-		.fail(function(data) {
-			// Make sure that the formMessages div has the 'error' class. 
-			$(formMessages).removeClass('success');
-			$(formMessages).addClass('error');
-
-			// Set the message text.
-			if (data.responseText !== '') {
-				$(formMessages).text(data.responseText);
-			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
-			}
-		});
-	});
-
+    const contactForm = $("#contact-form"),
+        formMessages = $('.form-messege');
+    contactForm.validate({
+        submitHandler: function(form){
+            $.ajax({
+                type: 'POST',
+                url: form.action,
+                data: $(form).serialize()
+            })
+            .done(function (response) {
+                formMessages.removeClass('error text-danger').addClass('success text-success mt-3').text(response);
+                // Clear the form.
+                form.reset()
+            })
+            .fail(function (data) {
+                // Make sure that the formMessages div has the 'error' class.
+                formMessages.removeClass('success text-success').addClass('error text-danger mt-3');
+                // Set the message text.
+                if (data.responseText !== '') {
+                    formMessages.text(data.responseText);
+                } else {
+                    formMessages.text('Oops! An error occured and your message could not be sent.');
+                }
+            });
+        }
+    });
 });
